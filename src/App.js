a511e26fbcc200afc79e331f4eb5a7e0e844c410
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-07-18 11:17:51 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import './app.scss';
+import Home from './pages/home/Home';
 function App() {
   return (
     <div className="app">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others">other pages</div>
+        <Home />
       </div>
     </div>
   );
