7c475f58659f75c0ddcb92f29b67d6cdcec65978
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-07-18 12:23:03 +0300

    implemented reusable chart

---
+++ src/dummyData.js
@@ -0,0 +1,55 @@
+export const userData = [
+  {
+    name: 'Jan',
+    user: 4000,
+  },
+  {
+    name: 'Feb',
+    user: 3000,
+  },
+  {
+    name: 'Mar',
+    user: 2000,
+  },
+  {
+    name: 'Apr',
+    user: 1890,
+  },
+  {
+    name: 'May',
+    user: 2390,
+  },
+  {
+    name: 'Jun',
+    user: 3490,
+  },
+
+  {
+    name: 'Jul',
+    user: 3490,
+  },
+
+  {
+    name: 'Aug',
+    user: 3290,
+  },
+
+  {
+    name: 'Sep',
+    user: 1490,
+  },
+
+  {
+    name: 'Oct',
+    user: 3340,
+  },
+
+  {
+    name: 'Nov',
+    user: 3690,
+  },
+  {
+    name: 'Dec',
+    user: 3650,
+  },
+];
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import { userData } from '../../dummyData';
 import './home.scss';
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart title="User Analytics" data={userData} grid dataKey="user" />
     </div>
   );
 };
